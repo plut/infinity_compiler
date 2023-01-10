e7d6b70bcbcbb7d0f9938088788135d1c31ce271
Jérôme Plût <plut.jerome@gmail.com>
2023-01-10 15:47:39 +0100

    derive(Unpack) is done

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
+#![allow(unused_imports,
+	unused_variables, dead_code,
+	unused_mut,
+	)]
 use std::path::Path;
 use std::fs::File;
 use std::io;
 use std::io::Read;
 use std::fmt;
-use std::fmt::Write;
+use std::cmp::min;
+// use std::fmt::Write;
 use resources::Unpack;
 
 mod pack;
-use pack::Pack;
+// use pack::Pack;
 
 mod res {
 use std::io;
@@ -21,120 +24,178 @@ use sqlite::Statement;
 struct Resref { name: i64, }
 struct Strref { value: i32, }
 #[derive(Debug)]
-enum FieldType { Integer, String, Resref, Strref }
+pub enum FieldType { Integer, String, Resref, Strref }
 #[derive(Debug)]
-struct Column<'a> {
-	fieldname: &'a str,
-	fieldtype: FieldType,
+pub struct Column<'a> {
+	pub fieldname: &'a str,
+	pub fieldtype: FieldType,
 }
 #[derive(Debug)]
 pub struct Schema<'a> {
-// 	fields: &'a[Column<'a>], // fat pointer
-	pub fields: &'a[&'a str], // fat pointer
+	pub fields: &'a[Column<'a>], // fat pointer
+// 	pub fields: &'a[&'a str], // fat pointer
 }
+pub trait Pack: Sized {
+	fn unpack(f: &mut impl Read)->io::Result<Self>;
+	fn pack(self, f: &mut impl io::Write)->io::Result<()> { unimplemented!() }
 
-// pub trait Pack: Sized {
-// 	fn unpack(f: &mut impl Read)->Result<Self>;
-// // 	fn pack(&self, f: &mut impl Write)->Result<()>;
-// }
-// 
-
+	fn unpack_header(f: &mut impl io::Read, hdr: &str)->io::Result<()> {
+		let mut buf = Vec::<u8>::with_capacity(hdr.len());
+		unsafe { buf.set_len(hdr.len()); }
+		f.read_exact(&mut buf)?;
+		println!(" found {buf:?}; is equal to hdr? {}", buf == hdr.as_bytes());
+		Ok(())
+	}
+// 	fn pack(&self, f: &mut impl Write)->Result<()>;
+}
+macro_rules! unpack_int {
+	($($T:ty),*) => { $(impl Pack for $T {
+			fn unpack(f: &mut impl Read)->io::Result<Self> {
+				let mut buf = [0u8; std::mem::size_of::<$T>()];
+				f.read_exact(&mut buf)?;
+				Ok(<$T>::from_le_bytes(buf))
+			}
+			fn pack(self, f: &mut impl Write)->io::Result<()> {
+				f.write_all(&self.to_le_bytes())
+			}
+	})* }
+}
+unpack_int!{i8,i16,i32,i64,u8,u16,u32,u64}
 pub trait Row {
 	type Key;
-	fn schema()->Schema<'static>;
+	const SCHEMA: Schema<'static>;
+// 	fn schema()->Schema<'static>;
 // 	fn bind(&self, s: &mut Statement<'_>, k: &Self::Key) -> Result<()>;
 // 	fn read(s: &mut Statement<'_>)->Result<(Self, Self::Key)>
 // 		where Self: Sized;
 }
 }
 
-#[derive(Debug)]
-struct StaticString<const N: usize>{
-	bytes: [u8; N],
-}
-
 use res::Row;
 use res::Schema;
+use res::Pack;
 
-impl<const N: usize> res::Row for StaticString<N> {
-	type Key = ();
-	fn schema()->Schema<'static> {
-		Schema{ fields: &["ab", "cd", "ef"] }
+#[derive(Debug)]
+struct StaticString<const N: usize>{ bytes: [u8; N], }
+impl<const N: usize> PartialEq<&str> for StaticString<N> {
+	fn eq(&self, other: &&str) -> bool {
+		for (i, c) in other.chars().enumerate() {
+			if i >= N { return false }
+			if !c.is_ascii() { return false }
+			if self.bytes[i] != (c as u8) { return false }
+			if c == '\0' { return true }
+		}
+		true
+	}
+}
+impl<const N: usize> From<&str> for StaticString<N> {
+// 	let s = StaticString::<8>::from("ABC");
+	fn from(s: &str) -> Self {
+		let s_bytes = s.as_bytes();
+		let mut bytes = [0u8; N];
+		for i in 0..min(s_bytes.len(), N-1) { bytes[i] = s_bytes[i]; }
+		Self { bytes: bytes, }
 	}
 }
 impl<const N: usize> std::fmt::Display for StaticString<N> {
-#[allow(unused_must_use)]
 	fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
 		std::fmt::Write::write_char(f, '"')?;
 		for c in &self.bytes {
-			*c == 0u8 && break;
+			if *c == 0u8 { break }
 			f.write_char(*c as char)?;
 		}
 		f.write_char('"')?;
+		f.write_fmt(format_args!("{}", N))?;
 		Ok(())
 	}
 }
-
-impl<const N: usize> pack::Pack for StaticString<N> {
+// impl<const N: usize> res::Row for StaticString<N> {
+// 	type Key = ();
+// // 	fn schema()->Schema<'static> {
+// // 		Schema{ fields: &["ab", "cd", "ef"] }
+// // 	}
+// }
+impl<const N: usize> res::Pack for StaticString<N> {
 	fn unpack(f: &mut impl Read)->io::Result<Self> {
 		let mut x = [0u8; N];
 		f.read_exact(&mut x)?;
 		Ok(Self{ bytes: x, })
 	}
+	fn pack(self, f: &mut impl Write)->io::Result<()> {
+		f.write_all(self.bytes)
+	}
+}
+impl<const N: usize> Default for StaticString<N> {
+	fn default()->Self { Self { bytes: [0u8; N] } }
 }
 
-/* macro must define the following:
- * - Pack and unpack methods
- * - a schema value (const if possible)
- * - bind to SQL statement (insert)
- * - take from SQL statement (select)
- *
- * XXX use derive(Pack) and derive(bind)
- */
+// pack::unpack!{ struct KeyHdr {
+// 	constant: StaticString<8>,
+// 	nbif: i32,
+// 	nres: i32,
+// 	bifoffset: u32,
+// 	resoffset: u32,
+// } }
 
-pack::unpack!{ struct KeyHdr {
+#[derive(Default,Debug)]
+struct KeyHdr {
 	constant: StaticString<8>,
 	nbif: i32,
 	nres: i32,
-	bifoffset: u32,
-	resoffset: u32,
-} }
+// 	bifoffset: u32,
+// 	resoffset: u32,
+}
 
-// macro_rules! test {
-// 	($a:ty) => { impl $a {
-// 		const fn schema()->&'static str { concat!("abcd", "xyz") }
-// 	} }
+// impl Row for KeyHdr {
+// 	type Key = i32;
+// 	const SCHEMA: Schema<'static> = Schema { fields: &[
+// 		res::Column{ fieldname: "nbif", fieldtype: res::FieldType::Integer} ] };
 // }
-// test!{Bytes8}
 
-use std::string::ToString;
-use std::iter::Iterator;
-fn foo(a: Vec<i32>)->String { format!("{:?}", a) }
-fn foo2(a: &Vec<i32>)->String {
-	a.into_iter()
-	.map(std::string::ToString::to_string)
-	.collect::<Vec<String>>()
-	.join(":")
-}
+// impl pack::Pack for KeyHdr {
+// 	fn unpack(mut f: &mut impl Read)->io::Result<Self> {
+// 		Ok(Self{constant: StaticString::<8>::unpack(&mut f)?,
+// 			nbif: i32::unpack(&mut f)?, nres: i32::unpack(&mut f)?,
+// 		.. Default::default() })
+// 	}
+// }
+// impl res::Pack for KeyHdr {
+// 	fn unpack(mut f: &mut impl Read)->io::Result<Self> {
+// 		Ok(Self{constant: StaticString::<8>::unpack(&mut f)?,
+// 			nbif: i32::unpack(&mut f)?, nres: i32::unpack(&mut f)?,
+// 		.. Default::default() })
+// 	}
+// }
 
-#[derive(Unpack)]
+// use std::string::ToString;
+// use std::iter::Iterator;
+// fn foo(a: Vec<i32>)->String { format!("{:?}", a) }
+// fn foo2(a: &Vec<i32>)->String {
+// 	a.into_iter()
+// 	.map(std::string::ToString::to_string)
+// 	.collect::<Vec<String>>()
+// 	.join(":")
+// }
+
+#[derive(Debug,Unpack)]
 struct Blah {
-	#[ign]
-	x: f64,
+	#[header("KEY V1  ")]
+	x: i32,
 }
-
 
 fn main() -> io::Result<()> {
-	Blah::say_hello();
-	println!("Hello, world!");
-	println!("{}", 2.to_string());
-	println!("{}", foo2(&vec![1,2,3]));
+// 	println!("s = {}", s);
+// 	println!("s == \"ABC\": {}, s==\"ABCDEFGHI\": {}", s == "ABC", s == "ABCDEFGHI");
+// 	println!("Hello, world!");
+// 	println!("{}", foo2(&vec![1,2,3]));
 	let gamepath = Path::new("/home/jerome/jeux/bg/game");
 	let index = gamepath.join("chitin.key");
 	println!("game index is {:#?}", index);
 	let mut f = File::open(index).expect("file not found");
-	let hdr = KeyHdr::unpack(&mut f)?;
-	println!("read succeeded buf: {:?}", hdr);
+	let x = Blah::unpack(&mut f)?;
+	println!("read Blah succeeded: {:?}", x);
+// 	let hdr = KeyHdr::unpack(&mut f)?;
+// 	println!("read succeeded buf: {:?}", hdr);
 // 	println!("schema is {}", Bytes8::schema());
 	Ok(())
 }
