547fdd8d47f1e8774aeb9d179030e27cfba36331
plut <plut@users.noreply.github.com>
2023-02-19 11:18:16 +0000

    deploy: 7bea2bb2e0050030e0a26736e9da7d3d2eba438e

---
+++ internals/debug/deps/macros-deba1f48b7a85c42.d
@@ -0,0 +1,5 @@
+/home/runner/work/infinity_compiler/infinity_compiler/guide/book/internals/debug/deps/libmacros-deba1f48b7a85c42.so: macros/src/lib.rs
+
+/home/runner/work/infinity_compiler/infinity_compiler/guide/book/internals/debug/deps/macros-deba1f48b7a85c42.d: macros/src/lib.rs
+
+macros/src/lib.rs:
